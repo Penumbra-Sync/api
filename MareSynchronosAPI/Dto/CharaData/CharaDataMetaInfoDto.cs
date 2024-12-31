effc2fa99eaac6892d75dc39f5418418c5bfa8dd
Stanley Dimant <root.darkarchon@outlook.com>
2024-12-31 14:30:31 +0100

    renaming

--- MareSynchronosAPI/Dto/CharaData/CharaDataMetaInfoDto.cs
+++ MareSynchronosAPI/Dto/CharaData/CharaDataMetaInfoDto.cs
@@ -3,7 +3,7 @@
 namespace MareSynchronos.API.Dto.CharaData;
 
 [MessagePackObject(keyAsPropertyName: true)]
-public record CharaDataMetaInfoDto(string Id, string UploaderUId) : CharaDataDto(Id)
+public record CharaDataMetaInfoDto(string Id, string UploaderUID) : CharaDataDto(Id)
 {
     public string Description { get; init; } = string.Empty;
     public DateTime UpdatedDate { get; init; }
